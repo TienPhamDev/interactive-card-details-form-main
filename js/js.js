409a60db54b44f4c1f52a6c0dc90af2e10f2c3ae
TienPhamDev <manhtienbn18891@gmail.com>
2022-12-14 21:57:42 -0500

    js

--- js/js.js
+++ js/js.js
@@ -1,1 +1,13 @@
 "use strict";
+const inputCardNameEl = document.querySelector(".form-card-name");
+const inputCardNumberEl = document.querySelector(".form-card-number");
+const inputCardMonthEl = document.querySelector(".form-card-month");
+const inputCardYearEl = document.querySelector(".form-card-yea");
+const confirmBtnEl = document.querySelector(".btn-confirm");
+const cardFrontNameEl = document.querySelector(".cards-front-name");
+
+inputCardNameEl.addEventListener("change", function () {
+  console.log(inputCardNameEl.value);
+  cardFrontNameEl.textContent = inputCardNameEl.value;
+  console.log(cardFrontNameEl);
+});
