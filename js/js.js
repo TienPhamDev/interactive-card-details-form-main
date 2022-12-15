c5a54a0dc34a26cbd283a2d14148d7fbfa6ab453
TienPhamDev <manhtienbn18891@gmail.com>
2022-12-15 17:17:19 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,13 +1,17 @@
 "use strict";
+// Sey varible for elements
 const inputCardNameEl = document.querySelector(".form-card-name");
 const inputCardNumberEl = document.querySelector(".form-card-number");
 const inputCardMonthEl = document.querySelector(".form-card-month");
 const inputCardYearEl = document.querySelector(".form-card-yea");
 const confirmBtnEl = document.querySelector(".btn-confirm");
 const cardFrontNameEl = document.querySelector(".cards-front-name");
+const cardFrontNumberEl = document.querySelector(".cards-front-number");
 
+//events listenner
 inputCardNameEl.addEventListener("change", function () {
-  console.log(inputCardNameEl.value);
-  cardFrontNameEl.textContent = inputCardNameEl.value;
-  console.log(cardFrontNameEl);
+  cardFrontNameEl.textContent = inputCardNameEl.value.toUpperCase();
+}); // Put input Name to Name Cards
+inputCardNumberEl.addEventListener("change", function () {
+  cardFrontNumberEl.textContent = inputCardNumberEl.value;
 });
