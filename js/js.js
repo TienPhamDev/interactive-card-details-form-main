0f63ae4f73eb8db7f3c022fcb9c117f4caaa68b7
TienPhamDev <manhtienbn18891@gmail.com>
2022-12-18 21:16:16 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 "use strict";
-// Sey varible for elements
+// Set varible for elements
 const inputCardNameEl = document.querySelector(".form-card-name");
 const inputCardNumberEl = document.querySelector(".form-card-number");
 const inputCardMonthEl = document.querySelector(".form-card-month");
@@ -7,11 +7,21 @@ const inputCardYearEl = document.querySelector(".form-card-yea");
 const confirmBtnEl = document.querySelector(".btn-confirm");
 const cardFrontNameEl = document.querySelector(".cards-front-name");
 const cardFrontNumberEl = document.querySelector(".cards-front-number");
-
+const warningEl = document.querySelector(".warning");
 //events listenner
 inputCardNameEl.addEventListener("change", function () {
   cardFrontNameEl.textContent = inputCardNameEl.value.toUpperCase();
 }); // Put input Name to Name Cards
 inputCardNumberEl.addEventListener("change", function () {
-  cardFrontNumberEl.textContent = inputCardNumberEl.value;
+  if (inputCardNumberEl.value.match(/[0-9]/g)) {
+    cardFrontNumberEl.textContent = `${inputCardNumberEl.value.slice(
+      0,
+      4
+    )} ${inputCardNumberEl.value.slice(4, 8)} ${inputCardNumberEl.value.slice(
+      8,
+      12
+    )} ${inputCardNumberEl.value.slice(12, 16)}`;
+  } else {
+    warningEl.classList.remove("hidden");
+  }
 });
