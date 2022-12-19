1bd8f2d9fa4c0387f64d9f136dbaf215def6b9d5
TienPhamDev <manhtienbn18891@gmail.com>
2022-12-19 00:44:24 -0500

    Update js.js

--- js/js.js
+++ js/js.js
@@ -8,20 +8,27 @@ const confirmBtnEl = document.querySelector(".btn-confirm");
 const cardFrontNameEl = document.querySelector(".cards-front-name");
 const cardFrontNumberEl = document.querySelector(".cards-front-number");
 const warningEl = document.querySelector(".warning");
+//function
+const cardNumberStrFormat = (str) =>
+  `${str.value.slice(0, 4)} ${str.value.slice(4, 8)} ${str.value.slice(
+    8,
+    12
+  )} ${str.value.slice(12, 16)}`; //this function to format numbers "0000 0000 0000 0000"
 //events listenner
 inputCardNameEl.addEventListener("change", function () {
   cardFrontNameEl.textContent = inputCardNameEl.value.toUpperCase();
-}); // Put input Name to Name Cards
+}); // from form Name card to Name Cards
 inputCardNumberEl.addEventListener("change", function () {
-  if (inputCardNumberEl.value.match(/[0-9]/g)) {
-    cardFrontNumberEl.textContent = `${inputCardNumberEl.value.slice(
-      0,
-      4
-    )} ${inputCardNumberEl.value.slice(4, 8)} ${inputCardNumberEl.value.slice(
-      8,
-      12
-    )} ${inputCardNumberEl.value.slice(12, 16)}`;
+  if (
+    inputCardNumberEl.value.match(/[0-9]/g) &&
+    inputCardNumberEl.value.length === 16
+  ) {
+    cardFrontNumberEl.textContent = cardNumberStrFormat(inputCardNumberEl);
+
+    inputCardNumberEl.value = cardNumberStrFormat(inputCardNumberEl);
+    console.log(inputCardNumberEl.textContent);
+    warningEl.classList.add("hidden");
   } else {
     warningEl.classList.remove("hidden");
   }
-});
+}); // from form card number to Card Number
